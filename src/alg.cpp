3f507420dea69082e68e6893c9646919d8dac6c5
Уваров Тимофей Максимович <66253074+timauvaroff@users.noreply.github.com>
2023-06-04 13:54:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,36 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int low = 0;
+    int high = size - 1;
+    
+    while (low <= high) {
+        int mid = low + (high - low) / 2;        
+        if (arr[mid] == value) {
+            count++;
+            for (int i = mid - 1; i >= low; i--) {
+                if (arr[i] == value)
+                    count++;
+                else
+                    break;
+            }
+            for (int i = mid + 1; i <= high; i++) {
+                if (arr[i] == value)
+                    count++;
+                else
+                    break;
+            }
+            
+            return count;
+        }
+        else if (arr[mid] < value) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    
+    return count;
 }
